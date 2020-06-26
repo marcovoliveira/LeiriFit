2d64ea981ff2a86c828d7359b49cce9d5c4aeeb8
user <marcooliveirapias@gmail.com>
2020-06-26 13:01:26 +0100

    Permissions

--- app/src/main/java/com/example/leirifit/MainActivity.kt
+++ app/src/main/java/com/example/leirifit/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.leirifit
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
@@ -11,7 +15,44 @@ import com.example.leirifit.databinding.ActivityMainBinding
 /**
  * Main activity binding, nav and fragments setup
  */
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
+
+
+    private val requiredPermissions: Array<String> by lazy {
+        try {
+            this.packageManager.getPackageInfo(
+                this.packageName,
+                PackageManager.GET_PERMISSIONS
+            ).requestedPermissions ?: arrayOf()
+        } catch (e: PackageManager.NameNotFoundException) {
+            arrayOf<String>()
+        }
+    }
+
+    private fun allPermissionsGranted() = requiredPermissions.none { !isPermissionGranted(it) }
+
+    private fun requestRuntimePermissions() {
+        val allNeededPermissions = requiredPermissions.filter { !isPermissionGranted(it) }
+
+        if (allNeededPermissions.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this,
+                allNeededPermissions.toTypedArray(),
+                PERMISSION_REQUESTS
+            )
+        }
+    }
+
+    private fun isPermissionGranted(permission: String): Boolean {
+        return when (ContextCompat.checkSelfPermission(this, permission)) {
+            PackageManager.PERMISSION_GRANTED -> {
+                true
+            }
+            else -> {
+                false
+            }
+        }
+    }
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var drawerLayout: DrawerLayout
@@ -26,10 +67,20 @@ class MainActivity : AppCompatActivity() {
         NavigationUI.setupWithNavController(binding.navView, navController)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
 
+        if (!allPermissionsGranted()) {
+            requestRuntimePermissions()
+        }
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
         return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
+    companion object {
+
+        private const val PERMISSION_REQUESTS = 1
+
     }
 }
