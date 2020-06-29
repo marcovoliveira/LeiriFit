754e782794ea88750ef3b844311d2add02497bee
Marco <marcooliveirapias@gmail.com>
2020-06-29 13:33:42 +0100

    colors and about

--- app/src/main/java/com/example/leirifit/AboutFragment.kt
+++ app/src/main/java/com/example/leirifit/AboutFragment.kt
@@ -1,9 +1,26 @@
 package com.example.leirifit
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.leirifit.databinding.FragmentAboutBinding
 
 /**
  * About fragment
  */
 class AboutFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentAboutBinding>(
+            inflater,
+            R.layout.fragment_about, container, false
+        )
+
+        return binding.root
+    }
 }
